2922ed153216957377c52b95efe981ca2067d580
Satoshi Watanabe <sassy.watanabe@gmail.com>
2024-11-22 12:47:47 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Macの環境構築スクリプト
+最初に実行して環境を構築するためのスクリプトです。
+Mac OSで最初にターミナルから実行してください。
+
+```
+$ chmod +x setup.sh
+$ ./setup.sh
+```
+
+以下に自分の開発環境のメモを置いておきます。
+
+--
+# 私の開発環境
+
+## エディタ
+- Visual Studio Code
+  - Awesome Emacs Keymap は入れる
+
+## ターミナル
+- iTerm2
+- zsh
+- tmux
+
+## 言語のバージョン管理
+- anyenv
+
+## 環境変数管理
+- direnv
+
+## コマンドラインツール
+- jq
+- gh
+
+## 開発系
+- Docker Desktop
+
+## ブラウザ
+Chrome
+
+### 拡張機能
+- はてなブックマーク
+- AWS Peacock Management Console
+- OneTab
+- Review Badge
+- BlockList
+
+## その他アプリ
+- Raycast
+ - Clipy使ってたけど、Raycastに移行
+- 1Password
+- Proxyman
+- AltTab
+- Figma
---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# zshのファイルを読み込む
+curl -fsSL -o .zshrc https://raw.githubusercontent.com/sassy/dotfiles/refs/heads/master/.zshrc
+source ~/.zshrc
+
+# homebrewをインストール
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade --all --cleanup
+brew -v
+
+softwareupdate --install-rosetta --agree-to-license
+
+# 必要なツール類
+brew install --cask google-chrome
+brew install --cask item2
+brew install --cask 1password
+brew install --cask docker
+brew install --cask raycast
+brew install --cask visual-studio-code
+brew install --cask slack
+brew install --cask onyx
+brew install --cask figma
+brew install --cask notion
+brew install --cask proxyman
+brew install --cask alt-tab
+
+brew install git
+brew install awscli
+brew install yarn
+brew install anyenv
+brew install direnv
+brew install jq
+brew install gh
+brew install tmux
+
+brew install --cask xcodes
+
+# anyenv update
+anyenv init
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+anyenv update
+
+# githubに接続するためのsshキーの作成(生成後にコピーしてgithubに設定)
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+ssh-keygen -t rsa
+chmod 400 ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa.pub
+ssh-add ~/.ssh/id_rsa
+touch ~/.ssh/config
+chmod 600 ~/.ssh/config
+echo "Host github
+  HostName github.com
+  IdentityFile ~/.ssh/id_rsa
+  User git
+  Port 22" >> ~/.ssh/config
+cd ../
+
